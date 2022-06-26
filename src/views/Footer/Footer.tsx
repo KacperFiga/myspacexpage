2de76ef8031dee9e8f9d5934472c0a199c065601
Kacper <63934835+KacperFiga@users.noreply.github.com>
2022-06-26 18:16:18 +0200

    first commit

---
+++ src/views/Footer/Footer.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledFooter = styled.footer`
+  background-color: "red";
+`;
+
+export const Footer = () => {
+  return (
+    <StyledFooter>
+    </StyledFooter>
+  );
+};
