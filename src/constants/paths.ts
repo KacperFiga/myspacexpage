b2caf7ce2ad14e772b84882e35db8394163c6d8d
Kacper <63934835+KacperFiga@users.noreply.github.com>
2022-07-01 23:32:12 +0200

    add basic API paths

--- src/constants/paths.ts
+++ src/constants/paths.ts
@@ -1,1 +1,7 @@
-export const PATH = "google.pl";
+const BASIC_URL = `https://api.spacexdata.com/v4`;
+export const UPCOMING_LAUNCHES_URL = `${BASIC_URL}/launches/upcoming`;
+export const NEXT_LAUNCH_URL = `${BASIC_URL}/launches/next`;
+export const LAST_LAUNCH_URL = `${BASIC_URL}/launches/latest`;
+export const PAST_LAUNCHES_URL = `${BASIC_URL}/launches/past`;
+export const COMPANY_INFO_URL = `${BASIC_URL}/company`;
+export const ROCKETS_URL = `${BASIC_URL}/rockets`;
